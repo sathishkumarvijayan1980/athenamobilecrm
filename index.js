4fb5aeb699511f22d96194081369640f5e818b07
sathishkumarvijayan1980 <vijayan.sathishkumar@hotmail.com>
2018-11-25 16:29:50 +0530

    Express changes to index file

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-// var http = require('http');
+var http = require('http');
 
 const express = require('express');
 
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 1337;
 app.use(cors())
 app.use(bodyParser.json());
 app.use(express.urlencoded({
     extended: false
 }));
 
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello Sai!");
+
+});
+
 app.listen(port, () => {
     console.info(`Mobile CRM API is @ ${port}`)
 })
-
-
-// var server = http.createServer(function(request, response) {
-
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World!");
-
-// });
 
 // var port = process.env.PORT || 1337;
 // server.listen(port);
